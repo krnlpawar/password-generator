4c6e267b2ae79228c1ef7b553fcd22e0eb2209fe
Krunal Pawar <krunal.pawar@neosoftmail.com>
2025-01-03 21:01:13 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -4,64 +4,88 @@ import { use } from 'react'
 import { useEffect } from 'react'
 
 function App() {
-  const [length, setLength] = useState(12)
-  const [specialChars, setSpecialChars] = useState(true)
-  const [numbers, setNumbers] = useState(true)
+  const [passwordLength, setPasswordLength] = useState(12)
+  const [includeSpecialCharacters, setIncludeSpecialCharacters] = useState(true)
+  const [includeNumbers, setIncludeNumbers] = useState(true)
 
-  const [password, setPassword] = useState("")
+  const [generatedPassword, setGeneratedPassword] = useState('')
+  const [showCopiedNotification, setShowCopiedNotification] = useState(false)
 
-  const generatePassword = () => {
-    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const generateRandomPassword = () => {
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
-    if (specialChars) {
-      characters += "!@#$%^&*()_+~`|}{[]\\"
+    if (includeSpecialCharacters) {
+      characters += '!@#$%^&*()_+~`|}{[]\\'
     }
-    if (numbers) {
-      characters += "0123456789"
+    if (includeNumbers) {
+      characters += '0123456789'
     }
-    let password = ""
-    for (let i = 0; i < length; i++) {
+
+    let password = ''
+    for (let i = 0; i < passwordLength; i++) {
       password += characters.charAt(Math.floor(Math.random() * characters.length))
     }
-    setPassword(password)
+
+    setGeneratedPassword(password)
   }
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(password)
-    setTimeOut(true)
+  const copyPasswordToClipboard = () => {
+    navigator.clipboard.writeText(generatedPassword)
+    setShowCopiedNotification(true)
     setTimeout(() => {
-      setTimeOut(false)
+      setShowCopiedNotification(false)
     }, 2000)
   }
 
-  const [timeOut, setTimeOut] = useState(false)
-
   useEffect(() => {
-    generatePassword()
-  }, [length, specialChars, numbers])
-  
+    generateRandomPassword()
+  }, [passwordLength, includeSpecialCharacters, includeNumbers])
+
   return (
     <>
       <div className="container">
         <h2>Password Generator</h2>
         <div className="password-settings">
-            { password && <p id="password">{password}</p>}
+          {generatedPassword && <p id="password">{generatedPassword}</p>}
 
-            <label for="length">Length:</label>
-            <input type="range" id="length" min="6" max="100" value={length} onChange={(e) => setLength(e.target.value)}/>
-            <span id="length-value">{length}</span>
-            <br/>
-            <label for="specialChars">Include Special Characters:</label>
-            <input type="checkbox" id="specialChars" value={specialChars} onChange={(e) => setSpecialChars(e.target.checked)} checked={specialChars}/>
-            <br/>
-            <label for="numbers">Include Numbers:</label>
-            <input type="checkbox" id="numbers" value={numbers} onChange={(e) => setNumbers(e.target.checked)} checked={numbers}/>
+          <label htmlFor="passwordLength">Length:</label>
+          <input
+            type="range"
+            id="passwordLength"
+            min="6"
+            max="100"
+            value={passwordLength}
+            onChange={(e) => setPasswordLength(e.target.value)}
+          />
+          <span id="passwordLengthValue">{passwordLength}</span>
+          <br />
+          <label htmlFor="includeSpecialCharacters">
+            Include Special Characters:
+          </label>
+          <input
+            type="checkbox"
+            id="includeSpecialCharacters"
+            value={includeSpecialCharacters}
+            onChange={(e) => setIncludeSpecialCharacters(e.target.checked)}
+            checked={includeSpecialCharacters}
+          />
+          <br />
+          <label htmlFor="includeNumbers">Include Numbers:</label>
+          <input
+            type="checkbox"
+            id="includeNumbers"
+            value={includeNumbers}
+            onChange={(e) => setIncludeNumbers(e.target.checked)}
+            checked={includeNumbers}
+          />
         </div>
-        <button id="generate" onClick={copyToClipboard}>Copy Password</button>
+        <button id="generate" onClick={copyPasswordToClipboard}>
+          Copy Password
+        </button>
         <br />
         <br />
-        {timeOut && <div id="copied">Password Copied to Clipboard</div>}
-    </div>
+        {showCopiedNotification && <div id="copied">Password Copied to Clipboard</div>}
+      </div>
     </>
   )
 }
